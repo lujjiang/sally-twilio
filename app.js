8bd9acb9ece8e8ef3880d952d1a17dffdba729f5
sally <lujjiang@cn.ibm.com>
2016-11-03 17:12:35 +0800

    update

--- app.js
+++ app.js
@@ -14,7 +14,37 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+var twilio = require('twilio');
+// Twilio Credentials
+var config = JSON.parse(process.env.VCAP_SERVICES);
+config['user-provided'].forEach(function(service){
+   if (service.name == 'Twilio') {
+     var accountSid = service.credentials.accountSID;
+     var authToken = service.credentials.authToken;
+   }
+}); 
+//var accountSid = 'ACbcebf97809d3a98688129a6fa7407a45'; 
+//var authToken = 'd1ff7fd62b52bf6825c0e2df3bcb6cd5'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+app.get('/message',function(req, res){
 
+	client.messages.create({ 
+    		to: "+8618614093682", 
+    		from: "+13372052748", 
+    		body: "This is the ship that made the Kessel Run in fourteen parsecs?", 
+	}, function(err, message) { 
+   		if(err){
+			console.log(err)
+			res.send('Err');
+		}
+   		else{ 
+   			console.log(message);
+			res.send('Message send! ID: '+message.sid);
+		} 
+	});
+});
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
